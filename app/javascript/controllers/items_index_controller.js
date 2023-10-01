413021893e6e107392fe6b5083be69877242d5a9
Harrison Hassig <harrisonhassig@gmail.com>
2023-10-01 23:10:55 +0800

    JS on items sort done

--- app/javascript/controllers/items_index_controller.js
+++ app/javascript/controllers/items_index_controller.js
@@ -6,12 +6,20 @@ export default class extends Controller {
     const items = document.querySelectorAll(".item-card");
     let categories = [];
     let categoriesHTML = document.querySelectorAll("#category");
+    //set "all" font-size on load
+    categoriesHTML[0].style["font-size"] = "1.5em";
     categoriesHTML.forEach((category) => {
       categories.push(category.innerText);
     });
     categoriesHTML.forEach((category) => {
       category.addEventListener("click", function() {
         displayAll(items);
+        resetCategories(categoriesHTML);
+        category.style["background-color"] = "white";
+        category.style["border-radius"] = "1em";
+        category.style["padding"] = "0.5em";
+        category.style["font-weight"] = "bold";
+        category.style["font-size"] = "1.5em";
         displayCategory(category, items);
       });
     });
@@ -19,7 +27,6 @@ export default class extends Controller {
       items.forEach((item) => {
         // hide cards that arent the same category as selected
         if (item.querySelector(".category-hidden").innerText !== category.innerText) {
-          console.log(item);
           item.style.display = "none";
         }
         if (category.innerText == "All") {
@@ -30,6 +37,15 @@ export default class extends Controller {
     function displayAll(items) {
       items.forEach((item) => {
         item.style.display = "block";
+      });
+    }
+    function resetCategories(categoriesHTML) {
+      categoriesHTML.forEach((category) => {
+        category.style["background-color"] = "white";
+        category.style["border-radius"] = "1em";
+        category.style["padding"] = "0.5em";
+        category.style["font-weight"] = "normal";
+        category.style["font-size"] = "1em";
       });
     }
   }
