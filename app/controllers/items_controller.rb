ec11ccf0c8c79deb48f564a5a74a6b3b595831e5
Harrison Hassig <harrisonhassig@gmail.com>
2023-10-05 10:04:21 +0800

    New/edit promotion form views for admin, plus destroy promotion

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,18 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
     @categories = Item.distinct.pluck(:category)
-    @transaction = Transaction.create(user_id: current_user ? current_user.id : 0)
+    # Check if user has an unpaid transaction and load that instead:
+    @transaction = current_user ? find_unpaid_transaction(current_user.id).first : Transaction.create(user_id: 0)
     @basket = Basket.new
   end
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  private
+
+  def find_unpaid_transaction(user_id)
+    Transaction.where(user_id: user_id, paid: false)
   end
 end
